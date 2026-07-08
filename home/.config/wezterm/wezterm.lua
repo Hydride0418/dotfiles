2a0ffdef4cf8460d9240c84e3ad4d19151e98dfd
yangjingping <yangjingping.ovo@bytedance.com>
2026-07-08 10:50:31 +0800

    Add WezTerm split pane shortcuts

--- home/.config/wezterm/wezterm.lua
+++ home/.config/wezterm/wezterm.lua
@@ -1,4 +1,5 @@
 local wezterm = require("wezterm")
+local act = wezterm.action
 
 local config = wezterm.config_builder()
 
@@ -12,5 +13,17 @@ config.window_background_opacity = 0.8
 config.macos_window_background_blur = 50
 config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "RESIZE"
+config.keys = {
+  {
+    key = "d",
+    mods = "CMD",
+    action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+  },
+  {
+    key = "d",
+    mods = "CMD|SHIFT",
+    action = act.SplitVertical({ domain = "CurrentPaneDomain" }),
+  },
+}
 
 return config
