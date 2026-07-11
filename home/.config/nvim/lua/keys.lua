66b8c8ed0c9dcc64a9646b52b447f883e61c1d19
yangjingping <yangjingping.ovo@bytedance.com>
2026-07-12 01:23:52 +0800

    fix: use uppercase keys for save and quit

--- home/.config/nvim/lua/keys.lua
+++ home/.config/nvim/lua/keys.lua
@@ -1,9 +1,9 @@
--- save without leaving the current mode
-vim.keymap.set({ 'n', 'i', 'x' }, '<C-s>', '<Cmd>write<CR>', { desc = 'Save' })
--- Ctrl-r normally means redo; keep that action available on Space+r
-vim.keymap.set('n', '<leader>r', '<C-r>', { desc = 'Redo' })
--- quit the current window, prompting when there are unsaved changes
-vim.keymap.set('n', '<C-r>', '<Cmd>confirm quit<CR>', { desc = 'Quit' })
+-- save and quit from normal mode
+vim.keymap.set('n', 'S', '<Cmd>write<CR>', { desc = 'Save' })
+vim.keymap.set('n', 'R', '<Cmd>confirm quit<CR>', { desc = 'Quit' })
+-- keep the original S (change line) and R (replace mode) commands available
+vim.keymap.set('n', '<leader>S', 'S', { desc = 'Change Line' })
+vim.keymap.set('n', '<leader>R', 'R', { desc = 'Replace Mode' })
 -- select all
 vim.keymap.set('n', '<C-a>', 'ggVG', { desc = 'Select All' })
 -- pasting over a selection no longer clobbers your clipboard
